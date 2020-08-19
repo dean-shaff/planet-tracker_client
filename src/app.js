aec432d9eb9f35d7dca09c41fdaf6a2b056c7520
Dean Shaff <dean.shaff@gmail.com>
2020-08-19 12:52:42 -0500

    small code formatting issue

--- src/app.js
+++ src/app.js
@@ -5,21 +5,21 @@ import App from "./components/App.vue"
 const version = "3.1.0"
 
 var init = ()=>{
-    var app = new Vue({
-        components:{
-            "app":App
-        },
-        data(){
-            return {
-                port: "5000",
-                host: document.domain,
-                "version": version
-            }
-        },
-        el: "#app",
-        template:`<app ref="app" :port="port" :host="host" :version="version"></app>`
-    })
-    return app
+  var app = new Vue({
+    components:{
+      "app":App
+    },
+    data(){
+      return {
+        port: "5000",
+        host: document.domain,
+        "version": version
+      }
+    },
+    el: "#app",
+    template:`<app ref="app" :port="port" :host="host" :version="version"></app>`
+  })
+  return app
 }
 
 var app = init()
